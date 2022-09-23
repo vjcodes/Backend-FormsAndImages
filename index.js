495b2d287a3d7d2f8526f7c3480cee1f93d68bc9
Vinayak Jaiswal <vinayak.kris@gmail.com>
2022-09-23 15:23:19 +0530

    - implemented forms - used cloudinary to upload one file and multiple files at a time

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
+
+const app = express()
+
+cloudinary.config({
+    // cloud_name: process.env.CLOUD_NAME
+    cloud_name: "dc4tylft8",
+    api_key:"498993394197816",
+    api_secret:"e0YYT50TcbLYYePQsMy0BuVfCf8"
+});
+
+app.set('view engine', 'ejs');
+
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(fileUpload({
+   useTempFiles: true,
+   tempFileDir: "/tmp/"
+}))
+
+app.get("/myget",(req, res) => {
+    console.log(req.query)
+    res.send(req.query)
+})
+
+app.post("/mypost", async(req, res) => {
+    console.log(req.body)
+    console.log(req.files);
+
+    let result;
+    let imageArray = [];
+
+    // case - multiple images
+
+    if (req.files) {
+        for (let index = 0; index < req.files.samplefile.length; index++) {
+           let result = await cloudinary.uploader.upload(req.files.samplefile[index].tempFilePath, {
+            folder: "users"
+           })
+
+           imageArray.push({
+            public_id: result.public_id,
+            secure_url: result.secure_url
+           })
+
+        }
+    }
+
+    // ### use case for single image
+
+    // let file = req.files.samplefile
+
+    // result = await cloudinary.uploader.upload(file.tempFilePath, {
+    //     folder: 'users'
+    // })
+
+    // console.log(result);
+
+    const details = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        // result,
+        imageArray
+    }
+    console.log(details);
+    res.send(details)
+})
+
+app.get("/mygetform",(req, res) => {
+    res.render('getform')
+})
+
+app.get("/mypostform",(req, res) => {
+    res.render('postform')
+})
+
+app.listen(4000, () => console.log('Server is running at PORT 4000'))
